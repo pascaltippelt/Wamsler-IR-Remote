44d62571fe0881037b0ac4c33b7ee168088f5d49
Pascal Tippelt <2066297+pascaltippelt@users.noreply.github.com>
2021-01-26 17:05:42 +0100

    Create telegram.h

---
+++ arduino_libary/src/telegram.h
@@ -0,0 +1,46 @@
+#ifndef telegram
+#define telegram
+
+#include <Arduino.h>
+
+
+
+
+class myTelegram
+{
+public:
+	myTelegram(int pin);
+	//Variablen
+	int OnOff = 0;									//On = 1, Off = 0
+	int AUTOTimer = 0;								//On = 1, Off = 0
+	int Turbo = 0;									//On = 1, Off = 0
+	int Econo = 0;									//On = 1, Off = 0
+	int timer1onActive = 0;							//True = 1, False = 0
+	int timer1offActive = 0;
+	int timer2onActive = 0;
+	int timer2offActive = 0;
+
+	//Funktionen, Set
+	bool setBlowerLevel(int level = 0);
+	bool setTemperature(int temperature = 20);
+	bool setTimer1on(int hour = 0, int minute = 0);
+	bool setTimer1off(int hour = 0, int minute = 0);
+	bool setTimer2on(int hour = 0, int minute = 0);
+	bool setTimer2off(int hour = 0, int minute = 0);
+	bool setTime(int hour = 0, int minute = 0);
+
+	//Funktionen, Output
+	String toString();
+	bool sendTelegram();
+	
+
+private:
+	bool data[72] = { 0 };
+	
+	bool calcTelegram();
+
+	bool calcPruefsumme();
+
+	int _pin = 0;
+};
+#endif
