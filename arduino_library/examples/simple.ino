731cf43bd96440ad026f47cf377940e1279924ec
Pascal Tippelt <2066297+pascaltippelt@users.noreply.github.com>
2021-01-26 17:14:01 +0100

    Create simple.ino

---
+++ arduino_library/examples/simple.ino
@@ -0,0 +1,30 @@
+#include <telegram.h>
+
+myTelegram t = myTelegram(13);
+
+void setup() {
+  t.AUTOTimer = true;
+  t.setBlowerLevel(3);
+  t.OnOff = true;
+  t.Turbo = false;
+  t.setTemperature(21);
+  t.setTimer1on(6, 30);
+  t.setTimer1off(11, 00);
+  t.setTimer2on(19, 00);
+  t.setTimer2off(21, 50);
+  t.setTime(20, 3);
+  t.timer1onActive = true;
+  t.timer1offActive = true;
+  t.timer2onActive = true;
+  t.timer2offActive = true;
+
+  Serial.begin(9600);
+  Serial.println(t.toString());
+  
+  t.sendTelegram();
+  
+  }
+
+void loop() {
+
+}
